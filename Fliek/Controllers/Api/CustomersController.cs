05bcd50e731ace922022e513b8269d8bd457b99f
Sweta Chauhan <sweta.chauhan14@gmail.com>
2020-02-04 12:14:28 -0600

    implemented CRUD using IHttpActionResult

--- Fliek/Controllers/Api/CustomersController.cs
+++ Fliek/Controllers/Api/CustomersController.cs
@@ -25,58 +25,58 @@ namespace Fliek.Controllers.Api
         }
 
         // GET /api/customers/1
-        public CustomerDto GetCustomers(int id)
+        public IHttpActionResult GetCustomers(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
-            {
-                throw new HttpResponseException(HttpStatusCode.NotFound);
-            }
-            return Mapper.Map<Customer,CustomerDto>(customer);
+                return NotFound();
+
+            return Ok(Mapper.Map<Customer,CustomerDto>(customer));
         }
 
         //POST /api/customers
         [HttpPost]
-        public CustomerDto CreateCustomer(CustomerDto customerDto)
+        public IHttpActionResult CreateCustomer(CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
-            {
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
-            }
+                return BadRequest();
+
             var customer = Mapper.Map<CustomerDto, Customer>(customerDto);
             _context.Customers.Add(customer);
             _context.SaveChanges();
             customerDto.Id = customer.Id;
-            return customerDto;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id), customerDto);
         }
 
         //PUT /api/customers/1
         [HttpPut]
-        public void UpdateCustomer(int id, CustomerDto customerDto)
+        public IHttpActionResult UpdateCustomer(int id, CustomerDto customerDto)
         {
             if (!ModelState.IsValid)
-            {
-                throw new HttpResponseException(HttpStatusCode.BadRequest);
-            }
+                return BadRequest();
             var customerDB = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customerDB == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+                return NotFound();
 
             Mapper.Map(customerDto, customerDB);
 
             _context.SaveChanges();
 
+            // return Ok(Mapper.Map<Customer, CustomerDto>(customerDB));
+            return Ok();
+
         }
 
         //DELETE /api/customers/1
         [HttpDelete]
-        public void DeleteCustomer(int id)
+        public IHttpActionResult DeleteCustomer(int id)
         {
             var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             if (customer == null)
-                throw new HttpResponseException(HttpStatusCode.NotFound);
+                return NotFound();
             _context.Customers.Remove(customer);
             _context.SaveChanges();
+            return Ok();
         }
     }
 }
